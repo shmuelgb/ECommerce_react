6e00f5523c98bbae069f1845c3b8587902880d3d
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2021-12-23 23:14:46 +0200

    color changed

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom/cjs/react-router-dom.min";
 export default function Homepage() {
   return (
     <div className="homepage">
-      <Link to="/products">Go to store</Link>
+      <Link to="/products">Go to store!</Link>
     </div>
   );
 }
